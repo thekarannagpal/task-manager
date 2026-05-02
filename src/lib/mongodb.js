0f0fbb2fe30c5d1dbc6b561f2c76cb6b6bff4a56
thekarannagpal <karannagpal2022@gmail.com>
2026-05-02 22:23:50 +0530

    Fix build error

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -1,11 +1,5 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI;
-
-if (!MONGODB_URI) {
-  throw new Error("Please define the MONGODB_URI environment variable inside .env.local");
-}
-
 let cached = global.mongoose;
 
 if (!cached) {
@@ -13,6 +7,12 @@ if (!cached) {
 }
 
 async function connectToDatabase() {
+  const MONGODB_URI = process.env.MONGODB_URI;
+
+  if (!MONGODB_URI) {
+    throw new Error("Please define the MONGODB_URI environment variable inside .env.local or your hosting provider.");
+  }
+
   if (cached.conn) {
     return cached.conn;
   }
